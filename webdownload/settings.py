28030c2dd6712ab0e8151d69ce5d17547501614b
jpablo <jose.p.alpaca@gmail.com>
2019-05-01 19:06:06 -0500

    signup form validations

--- webdownload/settings.py
+++ webdownload/settings.py
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'es-PE'
+LANGUAGE_CODE = 'en'
 
 TIME_ZONE = 'America/Lima'
 
